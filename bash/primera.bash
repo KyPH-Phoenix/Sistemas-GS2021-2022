6500c2ba64daf3ccc5e04f0528dee68bf5adcb0e
Hugo Notario <hugonotario29@gmail.com>
2022-01-31 19:57:59 +0100

    asdfasdfasdfasdf

--- bash/primera.bash
+++ bash/primera.bash
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-es_primo () {
-    if [ $1 -eq 1 ]
-    then 
+es_primo() {
+    if [ $1 -eq 1 ]; then
         return 0
     fi
 
-    for ((i = 2; i<$1; i++)); do
-        if [ $(( $1 % $i )) -eq 0 ]
-        then
+    for ((i = 2; i < $1; i++)); do
+        if [ $(($1 % $i)) -eq 0 ]; then
             return 1
         fi
     done
@@ -19,10 +17,9 @@ es_primo () {
 numero1=$1
 numero2=$2
 
- for ((j=$numero1; j<$numero2+1; j++)); do
+for ((j = $numero1; j < $numero2 + 1; j++)); do
     es_primo $j
-    if  [ $? -eq 0 ]
-    then
+    if [ $? -eq 0 ]; then
         echo $j
     fi
- done
+done
