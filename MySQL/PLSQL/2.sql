11e002e84fbbd72c1e5fbe83b342d0f6d4a2d178
Hugo Notario <hugonotario29@gmail.com>
2022-03-08 18:20:48 +0100

    2.sql

--- MySQL/PLSQL/2.sql
+++ MySQL/PLSQL/2.sql
@@ -1,16 +1,32 @@
 DROP DATABASE IF EXISTS exercicis_psql;
 CREATE DATABASE exercicis_psql;
-USE exercicis_psql; 
+USE exercicis_psql;
 
--- Enunciado o resumen de la función
-DROP FUNCTION IF EXISTS <funcion>;
+-- Modifiqueu el procediment dissenyat en l'exercici anterior
+-- perquè tingui un paràmetre d'entrada, amb el valor un nombre
+-- real, i un paràmetre de sortida, amb una cadena de caràcters 
+-- indicant si el número és positiu, negatiu o zero.
+
+DROP PROCEDURE  IF EXISTS sign;
+
 DELIMITER //
-CREATE FUNCTION <funcion>(<argumentos>)
-RETURNS <tipo> DETERMINISTIC
+CREATE PROCEDURE sign(IN num DECIMAL, OUT result varchar(20))
 BEGIN
-    -- Código
+    IF num > 0 THEN
+        SET result = "Es positivo";
+    ELSEIF num < 0 THEN
+        SET result = "Es negativo";
+    ELSE
+        SET result = "Es cero";
+    END IF;
 END //
 
+-- TEST --
 
-/* ----------- PRUEBAS ----------- */
-SELECT <funcion>(<argumentos>);
+SET @resultado="";
+CALL sign(-6, resultado);
+SELECT resultado;
+/* CALL sign(4, resultado);
+SELECT resultado;
+CALL sign(0, resultado);
+SELECT resultado; */
